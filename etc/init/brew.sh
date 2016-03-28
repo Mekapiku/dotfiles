89269c92f0583086f3de0f0e7d2e875763748d71
mot <mot.contact.address.pc@gmail.com>
2016-03-28 12:36:36 +0900

    homebrew周りのinit

---
+++ etc/init/brew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install command line tools
+if [[ ! -d /usr/include ]]; then
+  PLACEHOLDER=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  touch $PLACEHOLDER
+  PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+  softwareupdate -i "${PROD}"
+  [[ -f $PLACEHOLDER ]] && rm $PLACEHOLDER
+fi
+
+# install homebrew
+if [ ! `which brew` ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+cat `dirname $0`/brew_package_list.txt | while read line; do
+  brew install $line
+done
+
+brew update
+brew upgrade
